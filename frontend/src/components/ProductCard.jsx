6e20b7ecd6c6f7a3595b829929edffb8d73536c4
Tarek Elassady <tarekelassady@gmail.com>
2023-10-28 11:52:57 +0200

    m

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -8,22 +8,28 @@ import { AiFillHeart, AiOutlineHeart, AiOutlineEye, AiOutlineShoppingCart } from
 
 const ProductCard = ({ product }) => {
   const [getWishlist, setWishlist] = useState(false);
-  // console.log(product.title, product.variables[0].imgs);
+
   return (
-    <div className="products">
-      <div className="product" key={product._id}>
-        {product.variables.length>0 && product.variables[0].imgs.map(img => (
+    <div className="product" key={product._id}>
+      <Link to={`/product/${product.title.replace(/\s+/g, '-').toLowerCase()}`} state={product}><div className="product-details">
+        {product.variables.length > 0 && product.variables[0].imgs.map(img => (
           img.featured && <img src={img.url} alt={product.title} key={img.url} />
         ))}
-        <div className="action-buttons">
-          <AiOutlineShoppingCart style={{ fontSize: "18px" }} />
+        <div className="title">
+          {/* <AiOutlineShoppingCart style={{ fontSize: "18px" }} />
           <Link to={`/product/${product.title.replace(/\s+/g, '-').toLowerCase()}`} state={product}><AiOutlineEye style={{ fontSize: "18px" }} /></Link>
           {getWishlist ?
             <AiFillHeart style={{ fill: "red", fontSize: "18px" }} onClick={() => { setWishlist(!getWishlist) }} /> :
             <AiOutlineHeart style={{ fontSize: "18px" }} onClick={() => { setWishlist(!getWishlist) }} />
-          }
-        </div>
+          } */}
         <h3>{product.title.length > 40 ? product.title.slice(0, 40) + "..." : product.title}</h3>
+        </div>
+      </div></Link>
+      <div className="wishlist-actions">
+        {getWishlist ?
+          <AiFillHeart style={{ fill: "red", fontSize: "30px" }} onClick={() => { setWishlist(!getWishlist) }} /> :
+          <AiOutlineHeart className="remove-from-wishlist" style={{ fill: "var(--light-first-color)", fontSize: "30px" }} onClick={() => { setWishlist(!getWishlist) }} />
+        }
       </div>
     </div>
   )
